2c87f35acfdad689807c84f604f8e43014c9f262
Juan B. Rodriguez <jbrodriguez@gmail.com>
2018-03-29 10:24:30 -0500

    Implement City screen
    This is the first iteration.
    
    Although the design is pretty much set, there are a couple of things
    missing:
    
    - Map
    - Improve temperature unit display
    - Refresh the data
    
    A separate Gauge component displays temperatures in a user friendly
    way.
    
    Ref #3

--- src/colors/index.js
+++ src/colors/index.js
@@ -6,16 +6,18 @@ const palette = {
   primary: '#878D93', // baseText
   secondary: '#B9BCC0', // altText
   accent: '#ffffff', // accentText
+  muted: 'rgba(255,255,255,0.7)', // mutedText
 
   background: '#0F1B27',
   bgTone1: '#2F3B46',
   bgTone2: '#1A2733',
 
-  contrast: '#6D6345',
+  contrast: '#CBC52A',
 
   divider: '#1E2934',
 
   transparent: 'transparent',
+  white: '#ffffff',
 }
 
 const colors = adapter(palette)
