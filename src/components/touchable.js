565a655b79afeacf68bd5ceaa54364873b1e2981
Juan B. Rodriguez <jbrodriguez@gmail.com>
2018-03-29 05:17:46 -0500

    Add Touchable component
    This component uses the Android targeted API for touchables, while
    being seamless with iOS.
    
    Ref #3

---
+++ src/components/touchable.js
@@ -0,0 +1,43 @@
+// @flow
+
+import React, { PureComponent } from 'react'
+import { Platform, TouchableOpacity, TouchableNativeFeedback } from 'react-native'
+
+type Props = {
+  id: number,
+  disabled: boolean,
+  children: any,
+  goTo: (id: number) => () => void,
+}
+
+// This component uses the Android targeted API for touchables, while being seamless  with iOS
+export default class Touchable extends PureComponent<Props> {
+  render() {
+    const { id, disabled, children } = this.props
+
+    if (Platform.OS === 'ios') {
+      return (
+        <TouchableOpacity onPress={this.props.goTo(id)} disabled={disabled}>
+          {children}
+        </TouchableOpacity>
+      )
+    }
+
+    // Platform.OS === 'android'
+    const API21 = Platform.Version >= 21
+
+    return (
+      <TouchableNativeFeedback
+        onPress={this.props.goTo(id)}
+        disabled={disabled}
+        background={
+          API21
+            ? TouchableNativeFeedback.SelectableBackgroundBorderless()
+            : TouchableNativeFeedback.SelectableBackground()
+        }
+      >
+        {children}
+      </TouchableNativeFeedback>
+    )
+  }
+}
